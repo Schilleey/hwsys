7c5ca074f30ba9ecdc79f7d1c800aefc80f8fdf2
Patrick Link <patricklinkey@web.de>
2015-05-11 22:30:18 +0200

    Praktikum 2, Aufgabe 4

---
+++ ASM/p2a4.asm
@@ -0,0 +1,32 @@
+; Praktikum 2, Aufgabe 4
+; ----------------------
+
+; 0x100 => 0000 0001 0000 0000
+; 0x101 => 0000 0001 0000 0001
+; 0x102 => 0000 0001 0000 0010
+
+	  .org 0x000
+	  .start
+	  
+	  XOR   r0, r0, r0 ; Nur '0' in r0
+	  LDIH  r0, 0x01   ; r0 => 0x100
+	  LD    r1, [r0]   ; 1. Faktor in r1
+	  XOR   r0, r0, r0
+	  LDIL  r0, 0x01
+	  LDIH  r0, 0x01   ; r0 => 0x101
+	  LD    r2, [r0]   ; 2. Faktor in r2
+	  XOR   r0, r0, r0
+	  LDIL  r0, 0x02
+	  LDIH  r0, 0x01   ; r0 => 0x102
+	  XOR   r3, r3, r3
+	  XOR   r4, r4, r4
+	  LDIL  r4, 1
+	  XOR   r5, r5, r5
+	  LDIL  r5, loop
+	  LDIH  r5, loop>>8
+loop: ADD   r3, r3, r1 ; Addieren, r3 = r3 + r1
+	  SUB   r2, r2, r4
+	  JNZ   r2, r5
+	  ST    [r0], r3   ; Ergebnis speichern
+	  
+	  .end
