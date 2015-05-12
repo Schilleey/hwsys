2dee0440e6293f13ede2cfb881975d3605b7ad54
Thomas <studenttomcat@gmail.com>
2015-05-12 13:49:13 +0200

    kleines assembler

---
+++ ASM/smallTest.asm
@@ -0,0 +1,38 @@
+; Alle Befehle von ALU und LDIH und LDIL testen
+; ---------------------------------------------
+
+.org 0x000
+.start
+
+XOR   r0, r0, r0 ; Nur '0' in r0
+LDIL  r0, 8      ; 1. Wert in r0
+XOR   r1, r1, r1
+LDIL  r1, 4
+
+XOR   r3, r3, r3 ; Ergebnis Register leeren
+ADD   r3, r0, r1 ; Addieren, r3 = r0 + r1     => 12
+
+XOR   r3, r3, r3
+SUB   r3, r0, r1 ; Subtrahieren, r3 = r0 - r1 => 4
+
+XOR   r3, r3, r3
+SAL   r3, r0     ; Shift nach links           => 16
+
+XOR   r3, r3, r3
+SAR   r3, r0     ; Shift nach rechts          => 4
+
+XOR   r3, r3, r3
+LDIH  r3, 0xFF   ; => 1111111100000000
+LDIL  r3, 0xFF   ; => 1111111111111111
+AND   r3, r0, r1 ; => 0
+
+XOR   r3, r3, r3
+OR    r3, r0, r1 ; => 12
+
+XOR   r3, r3, r3
+XOR   r3, r0, r1 ; => 12
+
+XOR   r3, r3, r3
+NOT   r3, r0     ; => 1111111111110111
+
+.end
