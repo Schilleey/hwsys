1afb71a8479174fe77e573f021978c5d4769e824
Abszissex <Abszissex@users.noreply.github.com>
2015-05-21 13:26:58 +0200

    tried LD and ST and implement JMP, JZ, JNZ (hopefully...)

--- ASM/smallTest.asm
+++ ASM/smallTest.asm
@@ -41,4 +41,61 @@ XOR   r3, r0, r1 ; => 12
 XOR   r3, r3, r3
 NOT   r3, r0     ; => 1111111111110111
 
+;TODO!
+;LD
+XOR   r3, r3, r3
+XOR   r1, r1, r1
+LDIH  r1, 0xFF 
+LDIL  r1, 0xFF   ; => 1111111111111111
+LD  [r3], r1    ; => 1111111111111111
+
+;TODO!
+;ST
+XOR   r3, r3, r3
+XOR   r1, r1, r1
+LDIH  r1, 0xFF 
+LDIL  r1, 0xFF   ; => 1111111111111111
+ST  [r3], r1    
+
+
+;JMP
+XOR   r3, r3, r3
+XOR   r1, r1, r1
+LDIL r1, loopjmp
+LDIH r1, loopjmp>>8
+JMP r1
+LDIL r3, 0xFF
+loopjmp: ; r3 darf nicht gesetzt werden!
+
+
+;JNZ
+XOR   r3, r3, r3
+XOR   r1, r1, r1
+XOR   r2, r2, r1
+LDIL r2, 0x01
+LDIH r2, 0x00
+LDIL r1, loopjnz
+LDIH r1, loopjnz>>8
+JNZ r2, r1
+LDIL r3, 0xFF
+loopjnz: ; r3 darf nicht gesetzt werden!
+
+;JZ
+XOR   r3, r3, r3
+XOR   r1, r1, r1
+XOR   r2, r2, r1
+LDIL r2, 0x00
+LDIH r2, 0x00
+LDIL r1, loopjz
+LDIH r1, loopjz>>8
+JZ r2, r1
+LDIL r3, 0xFF
+loopjz: ; r3 darf nicht gesetzt werden!
+
+
+
+
+
+
+
 .end
